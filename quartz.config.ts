7bb0645c4b3937ca8eb8a0a10c728bff780d1905
hayes <hayes@buzzni.com>
2024-01-08 17:02:32 +0900

    Quartz sync: Jan 8, 2024, 5:02 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "리아의 여우정원",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
